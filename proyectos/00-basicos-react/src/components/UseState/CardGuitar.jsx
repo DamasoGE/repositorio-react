a6d10a79dae0d31d3e43359c6805c438bc98f299
DamasoGE <franciscodamasogimenez@gmail.com>
2025-01-13 08:49:57 +0100

    13-01-25-v0

---
+++ proyectos/00-basicos-react/src/components/UseState/CardGuitar.jsx
@@ -0,0 +1,34 @@
+import guitarsolid from "../../assets/guitar-solid.svg";
+import PropTypes from "prop-types";
+
+const CardGuitar = ({ guitar }) => {
+  const { name, price, type } = guitar;
+  return (
+    <div
+      key={name}
+      className="bg-gray-100 border p-4 mt-4 rounded-lg shadow-md flex items-center"
+    >
+      <img
+        src={guitarsolid}
+        alt={name}
+        width={80}
+        className="w-20 h-20 object-cover rounded-md  ml-4 mr-10"
+      />
+      <div>
+        <h2 className="text-lg font-bold">{name}</h2>
+        <p className="text-sm text-gray-700">{type}</p>
+        <p className="text-xl font-bold text-gray-700">{price} €</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardGuitar;
+
+CardGuitar.propTypes = {
+  guitar: PropTypes.shape({
+    name: PropTypes.string,
+    price: PropTypes.string,
+    type: PropTypes.string
+  })
+};
--- proyectos/00-basicos-react/src/components/UseState/ContinuacionNumeros.jsx
+++ proyectos/00-basicos-react/src/components/UseState/ContinuacionNumeros.jsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
 
-
 const ContinuacionNumeros = () => {
-    const [numbers, setNumbers] = useState([5,2,3,1,4])
-
-    const addNextNumber = () =>{
-        const maxNumber = Math.max(...numbers);
-
-        setNumbers((prevNumbers)=>[...prevNumbers,maxNumber+1]) //NUNCA REALIZAR PUSH
-    }
-
-    //función que añada el numero siguiente al ultimo numero del array (hay que ordenar los numeros)
-
+  const [numbers, setNumbers] = useState([5, 2, 3, 1, 4]);
+  // función que añada el número siguiente al último número del array (hay que ordenar los números)
+  const addNextNumber = () => {
+    const maxNumber = Math.max(...numbers);
+    setNumbers((prevNumbers) => [...prevNumbers, maxNumber + 1]);
+  };
 
   return (
     <>
-        <div>ContinuacionNumeros</div>
-        <button className="bg-slate-400 rounded mb-2 mx-2"
-        onClick={addNextNumber}>Añade el siguiente numero</button>
+      <div>
+        <button
+          className="bg-slate-400 rounded mb-2 mt-2"
+          onClick={addNextNumber}
+        >
+          {" "}
+          Añade el siguiente número
+        </button>
         <ul className="flex gap-2 my-2">
-            {numbers.map((number, index)=>{
-                <li key={index}></li>
-            })}
+          {numbers.map((number, index) => (
+            <li key={index}>{number}</li>
+          ))}
         </ul>
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default ContinuacionNumeros
+export default ContinuacionNumeros;
